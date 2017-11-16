7b141c204f58be900dbaba3a0421603c90747e9b
kingno21 <kingno21@gmail.com>
2017-11-16 19:16:18 +0900

    update products

--- src/app.js
+++ src/app.js
@@ -3,3 +3,32 @@ import './styles/app.scss';
 $('.navi#right').on('click', function () {
   $('.menu-body').toggleClass('active')
 });
+
+
+// google ana
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-104949268-1', 'auto');
+ga('send', 'pageview');
+
+
+// load style
+var cb = function() {
+  var l = document.createElement('link'); l.rel = 'stylesheet';
+  l.href = 'https://fonts.googleapis.com/css?family=Gloria+Hallelujah';
+  var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+};
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+  webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(cb);
+else window.addEventListener('load', cb);
